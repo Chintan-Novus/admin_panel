82c882cb999b80edf55353cd5d2af87f0a310d6f
Chintan Dave <chintandave.nl@gmail.com>
2021-09-17 19:27:42 +0530

    - Changes

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -1,49 +1,47 @@
 <?php
 
-Route::namespace('Novuslogics\AdminPanel\Http\Controllers')->group(function () {
 // Guest route
-    Route::middleware('guest')->group(function () {
-        // Login
-        Route::prefix('login')->group(function () {
-            Route::get('', [Auth\AuthenticatedSessionController::class, 'create'])->name('login');
-            Route::post('', [Auth\AuthenticatedSessionController::class, 'store']);
-        });
-
-        // Forgot password
-        Route::prefix('forgot-password')->as('password.')->group(function () {
-            Route::get('', [Auth\PasswordResetLinkController::class, 'create'])->name('request');
-            Route::post('', [Auth\PasswordResetLinkController::class, 'store'])->name('email');
-        });
-
-        // Reset password
-        Route::prefix('reset-password')->as('password.')->group(function () {
-            Route::get('{token}', [Auth\NewPasswordController::class, 'create'])->name('reset');
-            Route::post('', [Auth\NewPasswordController::class, 'store'])->name('update');
-        });
+Route::middleware('guest')->group(function () {
+    // Login
+    Route::prefix('login')->group(function () {
+        Route::get('', [Novuslogics\AdminPanel\Http\Controllers\Auth\AuthenticatedSessionController::class, 'create'])->name('login');
+        Route::post('', [Novuslogics\AdminPanel\Http\Controllers\Auth\AuthenticatedSessionController::class, 'store']);
     });
 
+    // Forgot password
+    Route::prefix('forgot-password')->as('password.')->group(function () {
+        Route::get('', [Novuslogics\AdminPanel\Http\Controllers\Auth\PasswordResetLinkController::class, 'create'])->name('request');
+        Route::post('', [Novuslogics\AdminPanel\Http\Controllers\Auth\PasswordResetLinkController::class, 'store'])->name('email');
+    });
+
+    // Reset password
+    Route::prefix('reset-password')->as('password.')->group(function () {
+        Route::get('{token}', [Novuslogics\AdminPanel\Http\Controllers\Auth\NewPasswordController::class, 'create'])->name('reset');
+        Route::post('', [Novuslogics\AdminPanel\Http\Controllers\Auth\NewPasswordController::class, 'store'])->name('update');
+    });
+});
+
 // Auth route
-    Route::middleware('auth')->group(function () {
-        // Logout
-        Route::post('/logout', [Auth\AuthenticatedSessionController::class, 'destroy'])->name('logout');
+Route::middleware('auth')->group(function () {
+    // Logout
+    Route::post('/logout', [Novuslogics\AdminPanel\Http\Controllers\Auth\AuthenticatedSessionController::class, 'destroy'])->name('logout');
 
-        // Dashboard
-        Route::view('dashboard', 'dashboard')->name('dashboard');
+    // Dashboard
+    Route::view('dashboard', 'dashboard')->name('dashboard');
 
-        // Account
-        Route::prefix('account')->as('account.')->group(function () {
-            // Profile
-            Route::prefix('profile')->as('profile.')->group(function () {
-                Route::get('edit', [ProfileController::class, 'edit'])->name('edit');
-                Route::put('', [ProfileController::class, 'update'])->name('update');
-                Route::put('dark_mode/change', [ProfileController::class, 'changeDarkMode'])->name('update.dark_mode');
-            });
+    // Account
+    Route::prefix('account')->as('account.')->group(function () {
+        // Profile
+        Route::prefix('profile')->as('profile.')->group(function () {
+            Route::get('edit', [Novuslogics\AdminPanel\Http\Controllers\ProfileController::class, 'edit'])->name('edit');
+            Route::put('', [Novuslogics\AdminPanel\Http\Controllers\ProfileController::class, 'update'])->name('update');
+            Route::put('dark_mode/change', [Novuslogics\AdminPanel\Http\Controllers\ProfileController::class, 'changeDarkMode'])->name('update.dark_mode');
+        });
 
-            // Change password
-            Route::prefix('change-password')->as('change_password.')->group(function () {
-                Route::get('', [ChangePasswordController::class, 'edit'])->name('edit');
-                Route::put('', [ChangePasswordController::class, 'update'])->name('update');
-            });
+        // Change password
+        Route::prefix('change-password')->as('change_password.')->group(function () {
+            Route::get('', [Novuslogics\AdminPanel\Http\Controllers\ChangePasswordController::class, 'edit'])->name('edit');
+            Route::put('', [Novuslogics\AdminPanel\Http\Controllers\ChangePasswordController::class, 'update'])->name('update');
         });
     });
 });
