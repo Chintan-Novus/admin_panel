f99885434d6d8edbc6c635783c1fe00df25e72de
Chintan Dave <chintandave.nl@gmail.com>
2021-09-17 18:57:43 +0530

    - Config changes

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -6,11 +6,17 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
 {
     public function register()
     {
-
+        // Config
+        $this->mergeConfigFrom(
+            __DIR__.'/../config/admin_panel.php', 'admin_panel'
+        );
     }
 
     public function boot()
     {
-
+        // Config
+        $this->publishes([
+            __DIR__.'/../config/admin_panel.php' => config_path('admin_panel.php'),
+        ]);
     }
 }
