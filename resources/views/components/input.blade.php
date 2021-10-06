351661721198b3bf4aeffa121bbc074ac424cf91
Chintan <chintandave.nl@gmail.com>
2021-10-06 16:26:41 +0530

    - Changes

--- resources/views/components/input.blade.php
+++ resources/views/components/input.blade.php
@@ -1,9 +1,8 @@
-@props(['type' => 'text', 'name', 'value' => null, 'placeholder' => null, 'id'])
+@props(['type' => 'text', 'name', 'value' => null, 'placeholder' => null])
 
 <input
     type="{{ $type }}"
     name="{{ $name }}"
-    id="{{ $id }}"
     placeholder="{{ ucfirst($placeholder) ?? '' }}"
     value="{{ $value }}" {!! $attributes->merge(['class' => 'form-control form-control-solid']) !!}
 />
