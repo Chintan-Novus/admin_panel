619cea4c2ae7269fdc08f5fc867fd419fc7c9104
Chintan <chintandave.nl@gmail.com>
2021-10-25 14:58:51 +0530

    - Changes

--- resources/views/components/switch.blade.php
+++ resources/views/components/switch.blade.php
@@ -1,9 +1,11 @@
-@props(['name', 'value'])
+@props(['name', 'value', 'checked' => null])
 
 <input
     type="checkbox"
     name="{{ $name }}"
     value="{{ $value ?? '' }}"
-    {!! $attributes->merge(['class' => 'form-check-input']) !!}/>
+    {!! $attributes->merge(['class' => 'form-check-input']) !!}
+    {{ $checked }}
+/>
 
 
