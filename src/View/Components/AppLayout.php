88cb300eaca90b5ab81ff536c0a2dad73de1b0c6
Chintan Dave <chintandave.nl@gmail.com>
2022-12-28 15:47:54 +0530

    Update AppLayout.php

--- src/View/Components/AppLayout.php
+++ src/View/Components/AppLayout.php
@@ -9,15 +9,17 @@ class AppLayout extends Component
     public $pageTitle;
     public $pageMeta;
     public $toolbarTitle;
+    public $toolbarDescription;
     public $toolbarButtons;
     public $toolbarDeleteAction;
 
-    public function __construct($pageTitle = null, $pageMeta = null, $toolbarTitle = null, $toolbarButtons = null, $toolbarDeleteAction = null)
+    public function __construct($pageTitle = null, $pageMeta = null, $toolbarTitle = null, $toolbarDescription = null, $toolbarButtons = null, $toolbarDeleteAction = null)
     {
         //
         $this->pageTitle = $pageTitle;
         $this->pageMeta = $pageMeta;
         $this->toolbarTitle = $toolbarTitle;
+        $this->toolbarDescription = $toolbarDescription;
         $this->toolbarButtons = $toolbarButtons;
         $this->toolbarDeleteAction = $toolbarDeleteAction;
     }
